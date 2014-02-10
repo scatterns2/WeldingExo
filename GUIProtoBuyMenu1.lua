ac41d890fa57cb0a333c69fa046c0e34f9ea8060
scatterns2 <normanrk@hotmail.com>
2014-02-10 22:24:34 +1100

    Update GUIProtoBuyMenu1.lua

--- GUIProtoBuyMenu1.lua
+++ GUIProtoBuyMenu1.lua
@@ -112,7 +112,7 @@ function GUIProtoBuyMenu:Initialize()
     
     self.modulePanelMap = {}   
     
-    self.modulePanelMap = {
+       self.modulePanelMap = {
         leftArm = {
             title = "LEFT ARM",
             dimensionData = { x = 10, y = 10, width = 40, height = 90 },
@@ -154,7 +154,7 @@ function GUIProtoBuyMenu:Initialize()
                 }
               },
           },
-          rightarm = {
+          rightArm = {
             title = "RIGHT ARM",
             dimensionData = { x = 100, y = 100, width = 50, height = 50 },
             moduleButtonDataList = {
@@ -192,26 +192,28 @@ function GUIProtoBuyMenu:Initialize()
                   normal = "ui/Egg.dds",
                   hover = "ui/Egg.dds",
                   selected = "ui/Egg.dds",
-                }
+                    }
+                },
             },
         },
-    }
-              
-    for panelName, panelData in pairs(self.modulePanelMap) do
-      
-           local panelBox = GUIManager:CreateGraphicItem()
+        },   
+     }
+
+    for panelName, panelData in pairs(self.modulePanelMap.leftArm) do
+            
+            local panelBox = GUIManager:CreateGraphicItem()
         panelBox:SetTexture( "ui/menu/repeating_bg.dds" )
         panelBox:SetColor(Color( 1, 0, 0, 1   ))
-        panelBox:SetPosition(Vector(panelData.dimensinData.x + panelData.dimensionData.y, 0 ))
-        panelBox:SetSize(Vector( panelData.width, panelData.height, 0 ))
+        panelBox:SetPosition(Vector(dimensionData.x + panelData.dimensionData.y, 0 ))
+        panelBox:SetSize(Vector( dimensionData.width, panelData.dimensionData.height, 0 ))
         //boxButton:AddChild(eggImage)
         panelBox:AddChild(panelText)
         self.content:AddChild(panelBox)
 
         table.insert(self.GUIItems, panelBox)
-        table.insert(self.modulePanelMap, panelBox)   
-      
-          local panelText = GUIManager:CreateTextItem()
+        table.insert(self.modulePanelMap, panelBox)  
+        
+         local panelText = GUIManager:CreateTextItem()
         panelText:SetFontName( "fonts/AgencyFB_small.fnt" )
         panelText:SetFontIsBold(true)
         panelText:SetPosition(Vector(10, 40, 0))
@@ -223,6 +225,10 @@ function GUIProtoBuyMenu:Initialize()
         //self.content:AddChild(eggButton)
   
         table.insert(self.GUIItems, panelText)
+        
+        
+      
+     
  
       for moduleButtonNum, moduleButtonData in ipairs(panelData.moduleButtonDataList) do
         
@@ -255,206 +261,7 @@ function GUIProtoBuyMenu:Initialize()
     end    
     
     
-    
-    
-   /* self.moduleData  = {}
-    
-    moduleData = {
-    leftArmModules = {
-                    [1] = {
-                        name = "Welder",
-                        available = false,
-                        powerCost = 20,
-                        weight = 20,
-                        buttonImages = {
-                            disabled = "materials/...",
-                            normal   = "materials/...",
-                            selected = "materials/...",
-                                        },
-                    },
-                    [2] = {
-                            name = "Shield",
-                        available = false,
-                        powerCost = 20,
-                        weight = 20,
-                        buttonImages = {
-                            disabled = "materials/...",
-                            normal   = "materials/...",
-                            selected = "materials/...",
-                                        },
-                    },
-                    [3] = {
-                        name = "Minigun",
-                        available = false,
-                        powerCost = 20,
-                        weight = 20,
-                        buttonImages = {
-                            disabled = "materials/...",
-                            normal   = "materials/...",
-                            selected = "materials/...",
-                                        },
-                    },
-                    [4] = {
-                        name = "Railgun",
-                        available = false,
-                        powerCost = 20,
-                        weight = 20,
-                        buttonImages = {
-                            disabled = "materials/...",
-                            normal   = "materials/...",
-                            selected = "materials/...",
-                                    },
-        },
-    },
-    rightArmModules = {
-                    [1] = {
-                        name = "Welder",
-                        available = false,
-                        powerCost = 20,
-                        weight = 20,
-                        buttonImages = {
-                            disabled = "materials/...",
-                            normal   = "materials/...",
-                            selected = "materials/...",
-                                        },
-                    },
-                    [2] = {
-                            name = "Shield",
-                        available = false,
-                        powerCost = 20,
-                        weight = 20,
-                        buttonImages = {
-                            disabled = "materials/...",
-                            normal   = "materials/...",
-                            selected = "materials/...",
-                                        },
-                    },
-                    [3] = {
-                        name = "Minigun",
-                        available = false,
-                        powerCost = 20,
-                        weight = 20,
-                        buttonImages = {
-                            disabled = "materials/...",
-                            normal   = "materials/...",
-                            selected = "materials/...",
-                                        },
-                    },
-                    [4] = {
-                        name = "Railgun",
-                        available = false,
-                        powerCost = 20,
-                        weight = 20,
-                        buttonImages = {
-                            disabled = "materials/...",
-                            normal   = "materials/...",
-                            selected = "materials/...",
-                                        },
-        },
-    }   
-}*/
-
-   /* buttonDataList[1] = { col = Color(1, 0, 0, 1), texture = "ui/egg.dds", text = "Claw"  }
-    buttonDataList[2] = { col = Color(1, 1, 1, 1), texture = "ui/egg.dds", text = "Welder"    }
-    buttonDataList[3] = { col = Color(1, 1, 1, 1), texture = "ui/egg.dds", text = "Minigun"  }
-    buttonDataList[4] = { col = Color(1, 1, 1, 1), texture = "ui/egg.dds", text = "Railgun"  }
-    buttonDataList[5] = { col = Color(1, 1, 1, 1), texture = "ui/egg.dds", text = "Flamethrower"  }
-    buttonDataList[6] = { col = Color(1, 0, 0, 1), texture = "ui/egg.dds", text = "Claw"  }
-    buttonDataList[7] = { col = Color(1, 1, 1, 1), texture = "ui/egg.dds", text = "Shield"    }
-    buttonDataList[8] = { col = Color(1, 1, 1, 1), texture = "ui/egg.dds", text = "Minigun"  }
-    buttonDataList[9] = { col = Color(1, 1, 1, 1), texture = "ui/egg.dds", text = "Railgun"  }
-    buttonDataList[10] = { col = Color(1, 1, 1, 1), texture = "ui/egg.dds", text = "Flamethrower"  }*/
-   
-    local eggsPerRow = 5
-
-    self.GUIItems = {}
-    self.buttonList = {}
-
-    
-    for buttonNum, buttonData in ipairs(buttonDataList) do
-
-        local eggImage = GUIManager:CreateGraphicItem()
-    
-        eggImage:SetPosition(Vector(10, 10, 0))
-        eggImage:SetColor( buttonData.col )
-        eggImage:SetTexture (buttonData.texture )
-        eggImage:SetSize(Vector( 25, 25, 0 ))
-        eggImage:SetTexturePixelCoordinates( 0, 0, 384, 192 )
-        //eggButton:SetText( buttonData.text)  
-       // eggButton:SetPosition(Vector(50+math.floor((buttonNum-1)/eggsPerRow)*35,50+ ((buttonNum-1)%eggsPerRow)*35, 0 ))
-        //self.content:AddChild(eggImage)
-
-        table.insert(self.GUIItems, eggImage)
-
-        
-        local testText = GUIManager:CreateTextItem()
-        testText:SetFontName( "fonts/AgencyFB_small.fnt" )
-        testText:SetFontIsBold(true)
-        testText:SetPosition(Vector(10, 40, 0))
-        testText:SetAnchor(        GUIItem.Left, GUIItem.Top        )
-        testText:SetTextAlignmentX(GUIItem.Align_Min)
-        testText:SetTextAlignmentY(GUIItem.Align_Max)
-        testText:SetColor(Color(kMarineFontColor))
-        testText:SetText(buttonData.text)
-        //self.content:AddChild(eggButton)
   
-        table.insert(self.GUIItems, testText)
-
-  
-        local boxButton = GUIManager:CreateGraphicItem()
-        boxButton:SetTexture( "ui/menu/repeating_bg.dds" )
-        boxButton:SetColor(Color(       1, 0, 0, 1   ))
-        boxButton:SetPosition(Vector(10+ ((buttonNum-1)%eggsPerRow)*150, 10+math.floor((buttonNum-1)/eggsPerRow)*150, 0 ))
-        boxButton:SetSize(Vector( 50, 50, 0 ))
-        boxButton:AddChild(eggImage)
-        boxButton:AddChild(testText)
-        self.content:AddChild(boxButton)
-
-        
-        table.insert(self.GUIItems, boxButton)
-        table.insert(self.buttonList, boxButton)
-
-        
-    end
-   
-   
-   
-    /*self.eggButtonList = {}
-
-    for buttonNum = 1, 50 do
-    
-        local eggButton = GUIManager:CreateGraphicItem()
-        
-        local eggsPerRow = 8
-
-        
-        eggButton:SetTexture( "ui/egg.dds" )
-        eggButton:SetPosition(Vector(50+ ((buttonNum-1)%eggsPerRow)*25, 50+math.floor((buttonNum-1)/eggsPerRow)*25, 0 ))
-        eggButton:SetColor( Color( 1, 1, 1, 1 ) )
-        eggButton:SetSize(Vector( 25, 25, 0 ))
-        eggButton:SetTexturePixelCoordinates( 0, 0, 384, 192 )
-        
-        self.content:AddChild(eggButton)
-
-     
-    table.insert(self.eggButtonList, eggButton)
-    
-    end*/
-    
-   /* do self.egg = GUIManager:CreateGraphicItem()
-             
-        self.egg:SetTexture( "ui/egg.dds" )
-        self.egg:SetPosition(Vector(100, 100, 0 ))
-        self.egg:SetColor( Color( 1, 1, 1, 1 ) )
-        self.egg:SetSize(Vector( 384*2, 192, 0 ))
-        self.egg:SetTexturePixelCoordinates( 0, 0, 384*2, 192 )
-
-
-
-       
-    end
-    self.content:AddChild(self.egg)*/
-    
     // START HERE
     
     
