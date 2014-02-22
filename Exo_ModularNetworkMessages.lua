8721aa3a9b862e9b2aa77b89530d7eea9b9ca9f2
scatterns2 <normanrk@hotmail.com>
2014-02-23 03:29:58 +1100

    Create Exo_ModularNetworkMessages.lua

---
+++ Exo_ModularNetworkMessages.lua
@@ -0,0 +1,11 @@
+ local kExoBuyMessage =
+{
+    leftArmModuleType = "enum kExoModuleTypes",
+    rightArmModuleType = "enum kExoModuleTypes",
+    powerModuleType = "enum kExoModuleTypes",
+    armorModuleType = "enum kExoModuleTypes",
+    utilityModuleType = "enum kExoModuleTypes"
+
+}
+
+Shared.RegisterNetworkMessage("ExoModularBuy", kBuyMessage)
